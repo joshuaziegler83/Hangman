8aec0cb2e398732506f638212dcbc5b0e71e1dd9
Joshua Ziegler <joshua.ziegler83@gmail.com>
2021-07-02 22:05:41 -0400

    Now with art

--- Hangman 3/Program.cs
+++ Hangman 3/Program.cs
@@ -12,19 +12,72 @@ namespace Hangman_3
         {
             int remainingGuesses = 6;
             string guessedLetters = "";
+            string[] asciiHangman = new string[7];
+            asciiHangman[6] = " +---+ \n" +
+                              " |   | \n" +
+                              "     | \n" +
+                              "     | \n" +
+                              "     | \n" +
+                              "     | \n" +
+                               "=========";
+
+            asciiHangman[5] = " +---+ \n" +
+                              " |   | \n" +
+                              " 0   | \n" +
+                              "     | \n" +
+                              "     | \n" +
+                              "     | \n" +
+                              "=========";
+
+            asciiHangman[4] = " +---+ \n" +
+                              " |   | \n" +
+                              " 0   | \n" +
+                              " |   | \n" +
+                              "     | \n" +
+                              "     | \n" +
+                              "=========";
+
+            asciiHangman[3] = " +---+ \n" +
+                              " |   | \n" +
+                              " 0   | \n" +
+                              "(|   | \n" +
+                              "     | \n" +
+                              "     | \n" +
+                              "=========";
+
+            asciiHangman[2] = " +---+ \n" +
+                              " |   | \n" +
+                              " 0   | \n" +
+                              "(|)  | \n" +
+                              "     | \n" +
+                              "     | \n" +
+                              "=========";
+
+            asciiHangman[1] = " +---+ \n" +
+                              " |   | \n" +
+                              " 0   | \n" +
+                              "(|)  | \n" +
+                              "]    | \n" +
+                              "     | \n" +
+                              "=========";
+
+            asciiHangman[0] = " +---+ \n" +
+                              " |   | \n" +
+                              " 0   | \n" +
+                              "(|)  | \n" +
+                              "] [  | \n" +
+                              "     | \n" +
+                              "=========";
+
+            
             var secretWord = new List<string> { "HANGMAN", "APPLE", "TOWER", "SMARTPHONE", "PROGRAMMING", "AWKWARD", "BANJO",
             "DWARVES", "FISHHOOK", "JAZZY", "JUKEBOX", "MEMENTO", "MYSTIFY", "OXYGEN", "PIXEL",
             "ZOMBIE", "NUMBSKULL", "BAGPIPES", "COMPUTER", "EASTER", "CHRISTMAS", "COFFEE" };
-            foreach (string word in secretWord)
-            {
-                Console.WriteLine(word);
-            }
-            Console.ReadLine();
             var random = new Random();
             int index = random.Next(secretWord.Count);
             string chosenWord = secretWord[index];
-            //Console.WriteLine(secretWord[index]);
-            //Console.ReadLine();
+            Console.WriteLine(secretWord[index]);
+            Console.ReadLine();
             string displayWord = "";
             foreach (char letter in chosenWord)
             {
@@ -36,11 +89,14 @@ namespace Hangman_3
             
             while (isGameRunning) 
             {
+                Console.Clear();
+                Console.WriteLine(asciiHangman[remainingGuesses]);
+                Console.WriteLine(displayWord);
                 Console.WriteLine("Enter A Guess: ");
+                Console.WriteLine($"Remaining Guesses: {remainingGuesses}");
+                Console.WriteLine($"Guessed Letters: {guessedLetters}");
                 letterGuess = char.Parse(Console.ReadLine());
-                //Console.WriteLine(letterGuess);
-                //Console.ReadLine();
-
+                letterGuess = char.ToUpper(letterGuess);
                 if (chosenWord.Contains(letterGuess))
                 {
                     for (int i = 0; i < chosenWord.Length; i++)
@@ -55,11 +111,11 @@ namespace Hangman_3
                     }
                     Console.WriteLine(displayWord);
                     string currentWord = displayWord.Replace(" ", "");
-                    if ( currentWord == chosenWord)
+                    if (currentWord == chosenWord)
                     {
                         Console.WriteLine("You guessed it! Congratulations!");
                         Console.ReadKey();
-                        isGameRunning = false; 
+                        isGameRunning = false;
                     }
                 }
                 else
@@ -67,24 +123,19 @@ namespace Hangman_3
                     if (!guessedLetters.Contains(letterGuess))
                     {
                         guessedLetters += (Char.ToString(letterGuess)) + " ";
-                        Console.WriteLine("Boo!");
                         remainingGuesses -= 1;
-                        Console.WriteLine($"Remaining Guesses: {remainingGuesses}");
-                        Console.WriteLine($"Guessed Letters: {guessedLetters}");
                         if (remainingGuesses == 0)
                         {
+                            Console.Clear();
+                            Console.WriteLine(asciiHangman[remainingGuesses]);
+                            Console.WriteLine($"The word was: {chosenWord}");
+                            Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("You've run out of guesses. Too bad!");
                             Console.ReadLine();
                             isGameRunning = false;
                         }
                     }
-                    else
-                    {
-                        Console.WriteLine("You've already guessed that letter.");
-                    }
-                    
                 }
-                //Console.ReadLine();
             }
         }
     }
